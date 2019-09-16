5e31a55bb4a23ae36fef178f9118aaad2558a9e3
Baktiyar Shakenov <coopkzgroup@gmail.com>
2019-09-16 22:37:54 +0600

    comm

---
+++ ajax/mail.php
@@ -0,0 +1,12 @@
+<?php
+  $email = $_POST['email']
+  $name = $_POST['name']
+  $phone = $_POST['phone']
+  $message = $_POST['message']
+
+  $subject = "=?utf-8?B?".base64_decode("Сообщение с сайта")."?=";
+  $headers = "From: $email\r\nContent-type: text/html; charset=utf-8\r\n"
+
+  $succes = mail("coopkzgroup@gmail.com", $subject,$message,$headers);
+  echo $success;
+?>
