dcd9aef19810b1c078c318cb873b1b0923a7bcc9
Baktiyar Shakenov <coopkzgroup@gmail.com>
2019-09-16 23:00:52 +0600

    Update mail.php

--- ajax/mail.php
+++ ajax/mail.php
@@ -4,7 +4,7 @@
   $phone = $_POST['phone'];
   $message = $_POST['message'];
 
-  $subject = "=?utf-8?B?".base64_decode("Сообщение с сайта")."?=";
+  $subject = "=?utf-8?B?".base64_encode("Сообщение с сайта")."?=";
   $headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
 
   $succes = mail("coopkzgroup@gmail.com", $subject,$message,$headers);
